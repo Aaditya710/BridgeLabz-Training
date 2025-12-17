9447ae924378b85646b3db5611ac0a1bd5060adb
Aaditya710 <aadityajoshi9826@gmail.com>
2025-12-17 14:57:57 +0530

    [Aaditya] : Added the program of Average Percent from level-1

---
+++ gcr-codebase/programming-element/Level-1-practice-program/AveragePercent.java
@@ -0,0 +1,10 @@
+public class AveragePercent{
+       public static void main(String [] args){
+              int mathsMarks = 94;
+              int physicsMarks = 95;
+              int chemistryMarks = 96;
+          
+              long percentage = (mathsMarks + physicsMarks + chemistryMarks) / 3;
+              System.out.println("Sam's average percent marks in PCM is " + percentage);
+}
+}

9fa5a65f54a23673fe55c2a1d3f67082b57552fa
Aaditya710 <aadityajoshi9826@gmail.com>
2025-12-17 14:59:24 +0530

    [Aaditya] : Added the km to miles conversion program from level-1

---
+++ gcr-codebase/programming-element/Level-1-practice-program/KilometerToMilesConversion.java
@@ -0,0 +1,9 @@
+public class KilometerToMilesConversion{
+       public static void main(String [] args){
+           double kilometers = 10.8;
+           double miles;
+           miles = kilometers * 1.6;
+           System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    
+}
+}

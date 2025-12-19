2f52ae7e1917f4b736611b99f2a9411fb789d51c
Aaditya710 <aadityajoshi9826@gmail.com>
2025-12-19 13:30:51 +0530

    [Aaditya] : Added the program to find sum of numbers

---
+++ gcr-codebase/arrays/Level-1/StoreNumbersAndSum.java
@@ -0,0 +1,34 @@
+// Creating StoreNumbersAndSum class to make to store numbers and their sum
+import java.util.Scanner;
+
+public class StoreNumbersAndSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] numbers = new double[10];
+        double total = 0.0;
+        int index = 0;
+        
+        // Taking input from user
+        while (true) {
+            System.out.print("Enter a number: ");
+            double input = sc.nextDouble();
+            if (input <= 0) {
+                break;
+            }
+            if (index == 10) {
+                break;
+            }
+            numbers[index] = input;
+            index++;
+        }
+
+        // Display numbers and calculate sum
+        System.out.println("Entered numbers:");
+        for (int i = 0; i < index; i++) {
+            System.out.println(numbers[i]);
+            total += numbers[i];
+        }
+        System.out.println("Sum of all numbers = " + total);
+    }
+}

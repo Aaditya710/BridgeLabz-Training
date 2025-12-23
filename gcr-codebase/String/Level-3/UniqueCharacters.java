af2fca2d430f51b50820976ef2418e27c6e43723
Aaditya710 <aadityajoshi9826@gmail.com>
2025-12-23 11:10:57 +0530

    [Aaditya] : Added the program of Unique charactes

---
+++ gcr-codebase/String/Level-3/UniqueCharacters.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class UniqueCharacters {
+
+    // Method to find length of a string without using length()
+    static int findLength(String text) {
+        int count = 0;
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            
+        }
+        return count;
+    }
+
+    // Method to find unique characters 
+    static char[] findUniqueCharacters(String text) {
+        int len = findLength(text);
+        char[] temp = new char[len]; 
+        int uniqueCount = 0;
+        for (int i = 0; i < len; i++) {
+            char ch = text.charAt(i);
+            boolean isUnique = true;
+            for (int j = 0; j < uniqueCount; j++) {
+                if (temp[j] == ch) {
+                    isUnique = false;
+                    break;
+                }
+            }
+
+            if (isUnique) {
+                temp[uniqueCount] = ch;
+                uniqueCount++;
+            }
+        }
+        char[] uniqueChars = new char[uniqueCount];
+        for (int i = 0; i < uniqueCount; i++) {
+            uniqueChars[i] = temp[i];
+        }
+        return uniqueChars;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        char[] uniqueChars = findUniqueCharacters(input);
+
+        // Displaying the output
+        System.out.print("Unique characters: ");
+        for (int i = 0; i < uniqueChars.length; i++) {
+            System.out.print(uniqueChars[i] + " ");
+        }
+    }
+}

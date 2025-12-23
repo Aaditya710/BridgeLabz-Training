5233a3288b434f09af243581da8581160b1e622c
Aaditya710 <aadityajoshi9826@gmail.com>
2025-12-23 11:07:32 +0530

    [Aaditya] : Added the program of first non repeating character

---
+++ gcr-codebase/String/Level-3/FirstNonRepeating.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class FirstNonRepeatingChar {
+
+    // Method to find the first non-repeating character
+    static char findFirstNonRepeatingChar(String text) {
+        int[] freq = new int[256]; 
+
+        // Count frequency of each character
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            freq[(int) ch]++;
+        }
+
+        // Find first character with frequency 1
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (freq[(int) ch] == 1) {
+                return ch;
+            }
+        }
+        return '\0';
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        char result = findFirstNonRepeatingChar(input);
+        if (result != '\0') {
+            System.out.println("First non-repeating character: " + result);
+        } else {
+            System.out.println("No non-repeating character found.");
+        }
+    }
+}

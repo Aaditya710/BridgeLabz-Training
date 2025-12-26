3dc8c548f3dc63e0c7668180afc5e1bd61077492
Aaditya710 <aadityajoshi9826@gmail.com>
2025-12-26 15:35:38 +0530

    [Aaditya] : added a program of Employee

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobjects/level1/Employee.java
@@ -0,0 +1,11 @@
+package com.classandobjects.level1;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("heyy world");
+
+	}
+
+}

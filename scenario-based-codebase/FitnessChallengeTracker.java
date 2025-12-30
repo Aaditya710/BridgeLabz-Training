a9fa398e20dfd00c5a9ecf773ab95291d8b20b40
Aaditya710 <aadityajoshi9826@gmail.com>
2025-12-30 13:18:55 +0530

    [Aaditya] : Added the program of fitness challange tracker

---
+++ scenario-based-codebase/FitnessChallengeTracker.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class FitnessChallengeTracker {
+
+    public static void main(String[] args) {
+
+        // Creating scanner class
+        Scanner sc = new Scanner(System.in);
+
+        int week =7; // 7 days
+        String weekdays[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int weekpushups[] = new int[7];
+
+        for(int i=0;i<week;i++){
+            if(i==2 || i==6){
+                System.out.println("Rest day"); // wednesday and sunday
+            }
+            else {
+                System.out.println("Enter push-ups count for "+ weekdays[i]);
+                weekpushups[i] = sc.nextInt();
+            }
+        }
+
+        // Calculating average pushups and total pushups
+        int totalPushups = 0;
+        for(int each : weekpushups) {
+           totalPushups+=each;
+        }
+
+        System.out.println("Total push-ups : "+totalPushups);
+        System.out.println("Average push-ups : " + totalPushups/5);
+    }
+}

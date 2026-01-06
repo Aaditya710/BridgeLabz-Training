b729444970b91e79a9d1ab4b6c14f6b174134b65
Aaditya710 <aadityajoshi9826@gmail.com>
2026-01-06 14:47:47 +0530

    [Aaditya] : Added the program of fit track

---
+++ java-oops-practice/scenario-based-codebase/OopsScenarioBasedPractice/src/com/fittrack/CardioWorkout.java
@@ -0,0 +1,15 @@
+package com.fittrack;
+
+class CardioWorkout extends Workout {
+
+    public CardioWorkout(int duration) {
+        super("Cardio", duration);
+    }
+
+    @Override
+    public double calculateCalories() {
+        caloriesBurned = duration * 8;  // higher burn
+        return caloriesBurned;
+    }
+}
+
